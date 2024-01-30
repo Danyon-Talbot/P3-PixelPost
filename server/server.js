db3ef8975cc1d6e6a8a85fd84704b8930e890a94
danyon talbot <danyon.talbot2@gmail.com>
2024-01-31 02:52:50 +1100

    Updating Server.js

--- server/server.js
+++ server/server.js
@@ -14,20 +14,28 @@ const server = new ApolloServer({
 
 const app = express();
 
-const startApolloServer = async () => {
-    await server.start();
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
-    app.use(express.urlencoded({ extended: false }));
-    app.use(express.json());
+// Apollo Server middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use('/graphql', expressMiddleware(server));
 
-    app.use('/graphql', expressMiddleware(server));
+// Handle all other routes by serving the 'index.html' file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+});
 
-    db.once('open', () => {
-        app.listen(PORT, () => {
-            console.log(`API server running on port ${PORT}`);
-            console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-        })
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}`);
+        console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
     })
-};
-
-startApolloServer();
+});
