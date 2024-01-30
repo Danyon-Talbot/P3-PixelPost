2fb75ba579b00c19d27bb7bbce4eb7e609554912
danyon talbot <danyon.talbot2@gmail.com>
2024-01-31 02:56:22 +1100

    Further Server.js testing

--- server/server.js
+++ server/server.js
@@ -1,41 +1,33 @@
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3007;
-const server = new ApolloServer({
-    typeDefs,
-    resolvers
-});
-
 const app = express();
 
 // Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
-// Apollo Server middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use('/graphql', expressMiddleware(server));
-
-// Handle all other routes by serving the 'index.html' file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+// Create an instance of Apollo Server
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
 });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ message: 'Internal Server Error' });
-});
+const startApolloServer = async () => {
+    await server.start();
 
-db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}`);
-        console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-    })
-});
+    server.applyMiddleware({ app }); // Use this middleware after server.start()
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}`);
+            console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+        });
+    });
+};
+
+startApolloServer();
