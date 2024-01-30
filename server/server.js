93e5fd6202e57af3e7c4930aab58de6b0232ca97
danyon talbot <danyon.talbot2@gmail.com>
2024-01-31 03:15:20 +1100

    BUGFIX Server.js

--- server/server.js
+++ server/server.js
@@ -12,30 +12,28 @@ const server = new ApolloServer({
     resolvers
 });
 
-const app = express();
-
-// Serve static files from the 'dist' directory
-app.use(express.static(path.join(__dirname, 'client/dist')));
-
-// Apollo Server middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use('/graphql', expressMiddleware(server));
-
-// Handle all other routes by serving the 'index.html' file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist/index.html'));
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ message: 'Internal Server Error' });
-});
-
-db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}`);
+const startApolloServer = async () => {
+    await server.start();
+  
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, '../client/dist')));
+      
+      app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      });
+    }
+    
+    app.use('/graphql', expressMiddleware(server));
+  
+    db.once('open', () => {
+      app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
         console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-    })
-});
+      });
+    });
+  };
+  
+  startApolloServer();
