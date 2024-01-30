a4b68c874468100b372689821a97cca961e03b37
danyon talbot <danyon.talbot2@gmail.com>
2024-01-31 02:35:11 +1100

    Testing Database Connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,6 +5,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pixelpost
   useUnifiedTopology: true,
 });
 
+// For Testing
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
