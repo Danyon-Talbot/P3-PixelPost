63bf8b043df556541dbff9a6d4777f5e3a52a714
danyon talbot <danyon.talbot2@gmail.com>
2024-01-31 17:59:11 +1100

    Successfully created user

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,12 @@
 import { gql } from '@apollo/client';
 
-const createUserMutation = gql`
+export const CREATE_USER_MUTATION = gql`
   mutation createUser($username: String!, $email: String!, $password: String!) {
     createUser(username: $username, email: $email, password: $password) {
       _id
       username
       email
+      # Include any other fields you want to retrieve
     }
   }
 `;
-
-export default createUserMutation;
