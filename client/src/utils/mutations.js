622a2d745170159b4c353e8f1e266283d7cca8a7
danyon talbot <danyon.talbot2@gmail.com>
2024-01-31 17:40:51 +1100

    testing Client Server Start

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+const createUserMutation = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export default createUserMutation;
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
-    port: 3007,
+    port: 3000,
     open: true,
     proxy: {
       '/graphql': {
