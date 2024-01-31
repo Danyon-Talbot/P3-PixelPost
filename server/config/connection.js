7b83eba412f8c087e74443b701164e8aa42547e5
danyon talbot <danyon.talbot2@gmail.com>
2024-01-31 14:21:12 +1100

    App Now Runs From Root

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pixelpost_db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pixelpost_db');
 
-// For Testing
+
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
