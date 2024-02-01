081e4564b5c6cf314b6e22ff53bd1422066e2882
danyon talbot <danyon.talbot2@gmail.com>
2024-02-02 00:46:58 +1100

    Removed Authenticated Routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,10 @@
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";;
-
-
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from './App.jsx';
 import Editor from './pages/Editor.jsx';
 import Login from './pages/Login.jsx';
 import Signup from './pages/Signup.jsx';
 import Profile from './pages/Profile.jsx';
-import RequireAuth from './utils/privateRoutes.js';
 
 const router = createBrowserRouter([
   {
@@ -17,27 +14,30 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Login />
-      }, {
+      },
+      {
         path: '/login',
         element: <Login />,
-      }, {
+      },
+      {
         path: '/signup',
         element: <Signup />,
-      }, 
+      },
       {
         path: '/editor',
-        element: <Editor />,
-      }, {
-        path: '/editor/:username',
-        element: <RequireAuth>
-          <Editor />
-          </RequireAuth>,
-      }, {
+        element:<Editor />,
+      },
+      {
+        path: '/editor/:username'
+      },
+      {
+        path: '/profile',
+        element: <Profile />,
+      },
+      {
         path: '/profile/:username',
-        element: <RequireAuth>
-          <Profile />
-          </RequireAuth>,
-      }
+        element: <Profile />,
+      },
     ]
   },
 ]);
